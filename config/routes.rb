a035dab0224e9643e764de25a047d36c8c965ffc
Rizwan Ali <rizcube@gmail.com>
2015-04-03 15:06:20 +0100

    rake:db migrate didn't work had to use  rake db:drop db:create db:migrate, finally user authentication is installed

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :statuses
+  resources :users
   root to: 'statuses#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
